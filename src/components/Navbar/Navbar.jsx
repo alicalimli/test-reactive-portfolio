7ec374153568f19d5aac008401e1c65e81075f9f
alicalimli <alicalimli76@gmail.com>
2022-06-07 10:51:25 +0300

    added nav-menu for responsive design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 import "./Navbar.scss";
 
 import { images } from "../../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(true);
+
   return (
     <nav className="app__navbar">
       <figure className="app__navbar-logo">
@@ -18,6 +22,24 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            {["home", "about", "work", "skills", "contact"].map((item) => (
+              <li className="app__flex p-text">
+                <a href={`#${item}`}>{item}</a>
+              </li>
+            ))}
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
