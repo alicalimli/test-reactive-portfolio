ae195d7d4cfb0e20b5288f61d6a0c4742a959385
alicalimli <alicalimli76@gmail.com>
2022-06-07 10:35:32 +0300

    added website logo and nav links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,24 @@ import React from "react";
 
 import "./Navbar.scss";
 
+import { images } from "../../constants";
+
 const Navbar = () => {
-  return <nav className="nav-bar"></nav>;
+  return (
+    <nav className="app__navbar">
+      <figure className="app__navbar-logo">
+        <img src={images.logo} alt="logo" />
+      </figure>
+      <ul className="app__navbar-links">
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
