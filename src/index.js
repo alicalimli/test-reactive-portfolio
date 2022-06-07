a39925303615ee854510aa3e33dbe215096a4588
alicalimli <alicalimli76@gmail.com>
2022-06-07 10:07:40 +0300

    fix scss not applying

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { createRoot } from "react-dom/cjs/react-dom.production.min";
-import { StrictMode } from "react/cjs/react.production.min";
+import * as ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+
 import App from "./App";
 import "./index.css";
 
 import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components/Index";
 
-import "./";
+import "./App.scss";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <div className="app">
